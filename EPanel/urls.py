f4a0537021602f4ab1ca9bf2cf3ee24d1d56c052
mahdia <mahdiaaghajanloo@gmail.com>
2019-12-24 18:11:44 +0330

    add to blacklist when logged-out

--- EPanel/urls.py
+++ EPanel/urls.py
@@ -19,7 +19,7 @@ from EPanel.core import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
     path('admin/', admin.site.urls),
