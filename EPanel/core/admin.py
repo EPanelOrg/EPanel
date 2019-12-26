6b9ed50e1b0563330aba0d416bf235712d05ce69
kimia <kimia.mousavi.98@gmail.com>
2019-12-26 15:53:15 +0000

    demand-supply

--- EPanel/core/admin.py
+++ EPanel/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from EPanel.core.models import Demand_supply
+# Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+admin.site.register(Demand_supply)
--- EPanel/core/models.py
+++ EPanel/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Demand_supply(models.Model):
+
+    home_id = models.IntegerField(primary_key=True)
+    demand = models.FloatField(default=0, null=True)
+    supply = models.FloatField(default=0, null=True)
