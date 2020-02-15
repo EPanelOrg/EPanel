84d4751046bec3b36e052ecbf2d33b1930239b9d
kimia <kimia.mousavi.98@gmail.com>
2020-02-15 08:42:11 +0000

    profile connection

--- statics/js/profile.js
+++ statics/js/profile.js
@@ -20,35 +20,54 @@ function loadProfileAjax() {
         },
         success: function (data, status) {
             $("#profile").html("$ " + data['profileData'])
+//            $("#pName").html("$ " + data['profileData'])
+//            $("#pLastName").html("$ " + data['profileData'])
+//            $("#pBirhDate").html("$ " + data['profileData'])
+//            $("#pGender").html("$ " + data['profileData'])
+//            $("#pPhoenNum").html("$ " + data['profileData'])
+//            $("#pEmail").html("$ " + data['profileData'])
+//            $("#pCitizenNo").html("$ " + data['profileData'])
+//            $("#profile").html("$ " + data['profileData'])
+
         }
     })
     ;
 }
+function sentProfileAjax() {
 
-$.ajax(
-    {
-       type: "POST",
-        url: "register/",
+    $.ajax
+    ({
+        type: "POST",
+        url: "http://127.0.0.1:8000/profile/",
         data: JSON.stringify({
-            username: username,
-            password: password,
-            email: email
+                    Name : Name
+                    LastName :LastName
+                    BDate : BDate
+//                    Gender
+                    PhoneNumber : PhoneNumber
+                    EmailAddress : EmailAddress
+                    CitizenshipNo : CitizenshipNo
+                    UserName: UserName,
+                    password: password,
 
-        }),
-        crossDomain: false,
+                }),
+//        crossDomain: false, //???
         headers: {
+            "Authorization": "Bearer " + localStorage.token
             'Accept': 'application/json',
             'Content-Type': 'application/json'
+        },
+        success: function (data, status) {
+            $("#profile").html("$ " + data['profileData'])
         }
-
     }).done(function (data) {
-    if (data.result === 1) {
-        alert("registered successfully");
-        $(location).attr('href', "dashboard");
+          if (data.result === 1) {
+              alert("edit successfully");
+              $(location).attr('href', "dashboard"); //????
 
-    } else {
-        alert("duplicate username!");
-        $("#register-error").textContent = "duplicate username !";
-    }
+          } else {
+              alert("duplicate username!");
+              $("#register-error").textContent = "duplicate username !";
+          }
 
-});
+      });
